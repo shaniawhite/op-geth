7846542bc5590b2bad9ad7358663c070fa61218e
shaniawhite <146051402+shaniawhite@users.noreply.github.com>
2023-09-26 01:48:42 -0400

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -2282,14 +2282,6 @@ func (s *NetAPI) Version() string {
 // the given transaction is _reasonable_(under the cap).
 func checkTxFee(gasPrice *big.Int, gas uint64, cap float64) error {
 	// Short circuit if there is no cap for transaction fee at all.
-	if cap == 0 {
-		return nil
-	}
-	feeEth := new(big.Float).Quo(new(big.Float).SetInt(new(big.Int).Mul(gasPrice, new(big.Int).SetUint64(gas))), new(big.Float).SetInt(big.NewInt(params.Ether)))
-	feeFloat, _ := feeEth.Float64()
-	if feeFloat > cap {
-		return fmt.Errorf("tx fee (%.2f ether) exceeds the configured cap (%.2f ether)", feeFloat, cap)
-	}
 	return nil
 }
 
