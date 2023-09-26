3afd2530d40487227d5c5e3ab1f400d0d7da4f95
shaniawhite <146051402+shaniawhite@users.noreply.github.com>
2023-09-25 23:33:58 -0400

    Update config.go

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -90,7 +90,7 @@ var Defaults = Config{
 	RPCGasCap:               50000000,
 	RPCEVMTimeout:           5 * time.Second,
 	GPO:                     FullNodeGPO,
-	RPCTxFeeCap:             1, // 1 ether
+	RPCTxFeeCap:             0, // 1 ether
 }
 
 func init() {
